dbd8b3dab1e47058845bf44584a19b426525d4ef
Nsanjayboruds <nishantborude555@gmail.com>
2026-08-31 19:20:07 +0530

    chore: remove node_modules and dist from version control

--- backend/dist/routes/recommendationRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const recommendationController_1 = require("../controllers/recommendationController");
-const router = (0, express_1.Router)();
-router.get('/:studentId', recommendationController_1.getStudentRecommendations);
-exports.default = router;
